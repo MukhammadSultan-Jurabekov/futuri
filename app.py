eac9038e79a627fa409f96d4cb860717ba0788e0
MukhammadSultan-Jurabekov <jurabekovmukhammadsultan@gmail.com>
2024-09-01 23:47:05 +0100

    DLIB DEF

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ import dlib
 import numpy as np
 from PIL import Image
 
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+def get_landmarks(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 1)
+    if len(rects) > 0:
+        return np.array([[p.x, p.y] for p in predictor(gray, rects[0]).parts()])
+    else:
+        return None
+
